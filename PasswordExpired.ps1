7457d7c6fa737723b95ef3b5f9a6c27b4e24955f
Or Guetta <ohg054@gmail.com>
2021-06-15 10:16:26 +0300

    Add files via upload

---
+++ PasswordExpired.ps1
@@ -0,0 +1,29 @@
+﻿import-module ActiveDirectory;
+
+$maxPasswordAgeTimeSpan = (Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge
+$WarnMsg = Get-Content \\Server\Files\password.htm -Raw
+$FromEmail=Password@Domain.Com
+$SmtpServer=Email.domian.com
+
+Get-ADUser -filter 'PasswordNeverExpires -ne $true' -properties PasswordLastSet, PasswordExpired, PasswordNeverExpires, EmailAddress, GivenName | foreach {
+ 
+    $today=get-date
+    $UserName=$_.GivenName
+    $Email=$_.EmailAddress
+    
+    if (!$_.PasswordExpired -and !$_.PasswordNeverExpires) {
+ 
+        $ExpiryDate=$_.PasswordLastSet + $maxPasswordAgeTimeSpan
+        $DaysLeft=($ExpiryDate-$today).days 
+ 
+        if ($DaysLeft -eq 21 -or $DaysLeft -eq 14 -or $DaysLeft -lt 8){
+ 
+        
+ForEach ($email in $_.EmailAddress) {  
+send-mailmessage -to $Email -from $FromEmail -Subject "Your password will be to expired in $daysleft days" -Body $WarnMsg -Encoding utf8 -SmtpServer $SmtpServer -BodyAsHtml  }
+
+
+            }
+ 
+    }
+}
